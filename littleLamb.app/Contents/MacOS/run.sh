0140a80ca4ca12b7f7f60a22e073f7ddd3a4ba1e
gwpjp <john.pearson.02@gmail.com>
2020-09-14 08:14:42 -0400

    Pushing final commits?

--- littleLamb.app/Contents/MacOS/run.sh
+++ littleLamb.app/Contents/MacOS/run.sh
@@ -5,6 +5,7 @@ open -a Terminal
 
 osascript <<END
 tell application "Terminal"
-    do script "clear;cd $dir/../../..; ./node_modules/.bin/babel littleLamb.js --out-file ./littleLamb.app/Contents/MacOS/littleLambES5.js; ./runJS.sh ./littleLamb.app/Contents/MacOS/littleLambES5.js"
+    do script "echo Hello?"
+    do script "cd $dir/../../..; echo File?; read var;echo $1; echo $var; ./node_modules/.bin/babel littleLamb.js --out-file ./littleLamb.app/Contents/MacOS/littleLambES5.js; ./runJS.sh ./littleLamb.app/Contents/MacOS/littleLambES5.js"
 end tell
 END
